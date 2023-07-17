7a60ebc1844752eea5f3504b92cca4b5833ec9ef
pgmrDohan <dohan.k764@andonghs.school.gyo6.net>
2023-07-17 10:53:54 +0900

    [ADD] footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="max"><a href="/"><typingTitle title="소프트웨어 개발자<br>권도한입니다."/></a></div>
+  <div class="max"><a href="/"><typingTitle
+    title="소프트웨어 개발자<br>권도한입니다."
+  /></a></div>
 
-  <titleContent title="print(“Hello, World!”)" msg="안녕하세요. 학생 개발자 권도한입니다.<br />다양한 언어를 다루면서 느리지만 확실하고 넓게 전진하고 있습니다.<br />프로그래밍 뿐만 아니라 보안이나 디자인에도 관심을 가지고 있어요." align="left">
+  <titleContent 
+    title="print(“Hello, World!”)"
+    msg="안녕하세요. 학생 개발자 권도한입니다.<br />다양한 언어를 다루면서 느리지만 확실하고 넓게 전진하고 있습니다.<br />프로그래밍 뿐만 아니라 보안이나 디자인에도 관심을 가지고 있어요."
+    align="left"
+  >
     <listContent emoji>
       <p class="leftA"><strong>Age</strong>16</p>
       <p class="leftA"><strong>Email</strong><a class="aAnimation" href="mailto:me@dohan.bio?subject=[Inquiry] ">me@dohan.bio</a></p>
@@ -14,26 +20,43 @@
     </listContent>
   </titleContent>
 
-  <titleContent title="편리와 도움. 능력향상과 최초." msg="다른 누군가에게 편리와 도움을 주는 것들을 만듭니다.<br />그러면서도 저 자신의 능력 향상과 최초라는 업적을 얻기 위해 노력합니다.<br />1석 4조인 셈이죠." align="right">
+  <titleContent
+    title="편리와 도움. 능력향상과 최초."
+    msg="다른 누군가에게 편리와 도움을 주는 것들을 만듭니다.<br />그러면서도 저 자신의 능력 향상과 최초라는 업적을 얻기 위해 노력합니다.<br />1석 4조인 셈이죠."
+    align="right"
+  >
   </titleContent>
   
-  <titleContent title="느리지만 꾸준히 성실하게 그리고 확실히" msg="CTF, 해커톤, 공모전 등 대회에 참여하고 실적을 쌓아갑니다.<br />여러 자격증도 하나 씩 얻고자 합니다.<br />착실히 앞으로 나아갑니다." align="left">
+  <titleContent
+    title="느리지만 꾸준히 성실하게 그리고 확실히"
+    msg="CTF, 해커톤, 공모전 등 대회에 참여하고 실적을 쌓아갑니다.<br />여러 자격증도 하나 씩 얻고자 합니다.<br />착실히 앞으로 나아갑니다."
+    align="left"
+  >
     <listContent emoji :height=40 :width=1517>
       <p><strong style="margin-right: 20px;">뭐지 뉴스래터 아티클 기고</strong>2023</p>
     </listContent>
     <listContent :height=40 :width=1517>
-      <p><strong style="margin-right: 20px;">뭐지 뉴스래터 아티클 기고</strong>2023</p>
+      <p><strong style="margin-right: 20px;">한국산업인력공단 정보처리기능사 자격증 취득</strong>2022</p>
     </listContent>
     <listContent :height=40 :width=1517>
-      <p><strong style="margin-right: 20px;">뭐지 뉴스래터 아티클 기고</strong>2023</p>
+      <p><strong style="margin-right: 20px;">한국과학창의재단 대학부설 과학영재 교육원 사사과정 연구 성과 발표대회 수상</strong>2022</p>
     </listContent>
     <listContent :height=40 :width=1517>
-      <p><strong style="margin-right: 20px;">뭐지 뉴스래터 아티클 기고</strong>2023</p>
+      <p><strong style="margin-right: 20px;">J2KB 전국연합코딩동아리 수료 및 인터뷰</strong>2021</p>
     </listContent>
   </titleContent>
   
-  <titleContent title="상상을 현실로 바꾸는 기술스택들" msg="필요한 여러 기술들을 배워갑니다.<br />많은 우물이지만 깊게 파내어 마스터하기 위해 노력합니다.<br />넘치는 생각들을 현실로 옮길 수 있도록 또 다른 우물을 파고 있습니다." align="right">
+  <titleContent 
+    title="상상을 현실로 바꾸는 기술스택들" 
+    msg="필요한 여러 기술들을 배워갑니다.<br />많은 우물이지만 깊게 파내어 마스터하기 위해 노력합니다.<br />넘치는 생각들을 현실로 옮길 수 있도록 또 다른 우물을 파고 있습니다."
+    align="right"
+  >
   </titleContent>
+
+  <div id="footer">
+    <a class="aAnimation" href="/files/DohanKwon_Resume.pdf" target='_blank'><p>PDF Version</p></a>
+    <p>Copyright 2023. Dohan all rights reserved.</p>
+  </div>
 </template>
 
 <script>
@@ -66,7 +89,7 @@ export default {
   align-items : center;
 }
 
-.max a {text-decoration: none; color: inherit;}
+a {text-decoration: none; color: inherit;}
 
 .aAnimation {
   display: inline-block;
@@ -106,4 +129,12 @@ div {
   float: left;
 }
 
+#footer {
+  text-align: center;
+  margin-top: 150px;
+  margin-bottom: 30px;
+  font-family: Pretendard Variable, sans-serif;
+  font-size: 13px;
+  font-weight:300;
+}
 </style>
