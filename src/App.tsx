3c7da570aae096e1ab0624ba5870f7849f7481d3
권도한 <dohan@gwondohans-MacBook-Pro.local>
2024-07-19 00:32:06 +0900

    [MODIFY] responsible

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ import Tag from './components/Tag';
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			<Triangle size={200} color="#3894FF" style={{ position: "absolute", top: "230px", rotate: "8.71deg", left: "570px", zIndex: "-10" }} />
-			<Tag content="Portfolio" style={{ position: "absolute", left: "800px", rotate: "10.62deg", top: "280px", zIndex: "-10" }} />
-			<Tag content="LowLevel" style={{ position: "absolute", left: "430px", rotate: "-8.58deg", top: "520px", zIndex: "-10" }} />
-			<Circle size={100} color="#38FF70" style={{ position: "absolute", left: "1010px", top: "500px", zIndex: "-10" }} />
-			<Tag content="TechBlog" style={{ position: "absolute", left: "1080px", rotate: "8.48deg", top: "620px", zIndex: "-10" }} />
-			<Circle size={100} color="#FFF738" style={{ position: "absolute", left: "270px", top: "740px" }} />
-			<Tag content="UI/UX" style={{ position: "absolute", left: "290px", rotate: "-16.67deg", top: "730px", zIndex: "-11" }} />
-			<Tag content="Web Fullstack" style={{ position: "absolute", left: "800px", rotate: "21.06deg", top: "760px", zIndex: "-11" }} />
-			<Circle size={100} color="#FF3838" style={{ position: "absolute", left: "750px", top: "770px" }} />
-			<Tag content="Dohan Kwon" style={{ position: "absolute", top: "870px", left: "470px", zIndex: "-12" }} />
+			<Header mail='me@dohan.in' github='pgmrdohan' insta='i70h4n' />
+			{/* <Triangle size={150} color="#3894FF" style={{ rotate: "8.71deg", zIndex: "-10" }} /> */}
+			<Tag content="Portfolio" style={{ rotate: "10.62deg", zIndex: "-10", right: "180px", bottom: "500px" }} />
+			<Tag content="LowLevel" style={{ rotate: "-8.58deg", zIndex: "-10", bottom: "300px", left: "80px" }} />
+			{/* <Circle size={100} color="#38FF70" style={{ position: "absolute", left: "1010px", top: "500px", zIndex: "-10" }} /> */}
+			<Tag content="TechBlog" style={{ rotate: "8.48deg", zIndex: "-10", right: "6px", bottom: "250px" }} />
+			{/* <Circle size={100} color="#FFF738" style={{ position: "absolute", left: "270px", top: "740px" }} /> */}
+			<Tag content="UI/UX" style={{ rotate: "-16.67deg", zIndex: "-11", left: "1px", bottom: "150px" }} />
+			<Tag content="Web Fullstack" style={{ rotate: "21.06deg", zIndex: "-11", right: "1px", bottom: "120px" }} />
+			{/* <Circle size={100} color="#FF3838" style={{ position: "absolute", left: "770px", top: "790px" }} /> */}
+			<Tag content="Dohan Kwon" style={{ zIndex: "-12", bottom: "50px", left: "15%" }} />
 		</div>
 	);
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from '../../assets/logo.png';
 import { MdEmail } from "react-icons/md";
 import { VscGithubInverted } from "react-icons/vsc";
@@ -5,7 +6,13 @@ import { BsInstagram } from "react-icons/bs";
 import silinder from '../../assets/silinder.svg';
 import './styles.scss';
 
-export default function Header() {
+export interface Props {
+	mail: string,
+	github: string,
+	insta: string
+}
+
+export default function Header(props: Props) {
 	return (
 		<div className="Header">
 			<img src={silinder} alt="" className='silinder' />
@@ -13,9 +20,9 @@ export default function Header() {
 			<div className='content'>
 				<img id='logo' src={logo} alt="" />
 				<div className='sns'>
-					<MdEmail style={{ color: "white" }} />
-					<VscGithubInverted style={{ color: "white" }} />
-					<BsInstagram style={{ color: "white" }} />
+					<a href={"mailto:" + props.mail}><MdEmail style={{ color: "white" }} /></a>
+					<a href={"https://github.com/" + props.github} target="_blank" rel="noreferrer"><VscGithubInverted style={{ color: "white" }} /></a>
+					<a href={"https://instagram.com/" + props.insta} target="_blank" rel="noreferrer"><BsInstagram style={{ color: "white" }} /></a>
 				</div>
 			</div >
 		</div >
