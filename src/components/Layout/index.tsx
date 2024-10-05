cb37e38be98a19cd242b373be54c55a1e15c492f
권도한 <dohan@gwondohans-MacBook-Pro.local>
2024-10-06 01:16:46 +0900

    feat: Dynamic Meta tag

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,36 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../Footer";
 import { Header } from "../Header"
+import { Head } from 'vite-react-ssg'
 import styles from './index.module.scss'
+import { MainEmbedImage } from "@/assets";
+import { useEffect, useState } from "react";
 
 export const Layout = () => {
+	const [location, setLocation] = useState<{ pathname: string, href: string }>({ pathname: "", href: "" });
+
+	useEffect(() => {
+		setLocation({
+			pathname: window.location.pathname,
+			href: window.location.href
+		});
+	}, [])
+
 	return (
 		<>
+			<Head>
+				<title>Dohan Kwon - {location.pathname === '/' ? "Portfolio" : location.pathname}</title>
+				<meta name="title" content={`Dohan Kwon - ${location.pathname === '/' ? "Portfolio" : location.pathname}`} />
+				<meta name="description" content={"I'm Dohan Kwon, a developer who connects people to people and people to things. I started developing software in Korea in 2020 and has been growing my tech stack and developing various services. Currently, I works as an individual freelance developer in various fields."} />
+				<meta property="og:url" content={location.href} />
+				<meta property="og:title" content={`Dohan Kwon - ${location.pathname === '/' ? "Portfolio" : location.pathname}`} />
+				<meta property="og:description" content={"I'm Dohan Kwon, a developer who connects people to people and people to things. I started developing software in Korea in 2020 and has been growing my tech stack and developing various services. Currently, I works as an individual freelance developer in various fields."} />
+				<meta property="og:image" content={location.pathname === '/' ? MainEmbedImage : ""} />
+				<meta property="twitter:url" content={location.href} />
+				<meta property="twitter:title" content={`Dohan Kwon - ${location.pathname === '/' ? "Portfolio" : location.pathname}`} />
+				<meta property="twitter:description" content={"I'm Dohan Kwon, a developer who connects people to people and people to things. I started developing software in Korea in 2020 and has been growing my tech stack and developing various services. Currently, I works as an individual freelance developer in various fields."} />
+				<meta property="twitter:image" content={location.pathname === '/' ? MainEmbedImage : ""} />
+			</Head>
 			<Header className={styles.Header} />
 			<div className={styles.Content}>
 				<Outlet />
