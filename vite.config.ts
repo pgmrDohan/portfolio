2d56ac4d108d329a308acef698a78a1a49cefb07
권도한 <dohan@gwondohans-MacBook-Pro.local>
2024-10-06 02:23:13 +0900

    delete: dynamic meta tag

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 	cacheDir: ".yarn/.vite",
 	ssgOptions: {
 		crittersOptions: false,
-		entry: "src/index.ts"
+		entry: "src/index.ts",
 	},
 	optimizeDeps: {
 		exclude: ['blip-ds/loader'],
